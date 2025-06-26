e0fbebee1f6e1de4a5eb215f88e636cd881ea69d
fdgbatarse1 <fdgbatarse@gmail.com>
2025-06-26 02:02:11 -0700

    :sparkles: Add mlflow

--- airflow/src/mlflow_config.py
+++ airflow/src/mlflow_config.py
@@ -2,5 +2,6 @@
 import mlflow
 
 def setup_mlflow():
-    mlflow.set_tracking_uri("file:./mlruns")
+    MLFLOW_TRACKING_URI = "http://localhost:5001"
+    mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
     mlflow.set_experiment("TestVersioning")
